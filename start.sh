ef8294b5e6339881f397bc97719e57a54fb00b8d
Moussa <moussa@macbook-air-de-moussa.home>
2026-01-05 08:33:22 +0100

    Add debug output to start script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo "Starting Next.js on port $PORT..."
+echo "=== RootsRemedy Server Starting ==="
+echo "PORT: $PORT"
+echo "NODE_ENV: $NODE_ENV"
+echo "Starting Next.js..."
 exec node_modules/.bin/next start -H 0.0.0.0 -p ${PORT:-3000}
